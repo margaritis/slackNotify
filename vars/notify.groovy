d0f2d9afed6438f124cdca5c8899c4c2977edc17
Konstantinos Margaritis <kotsos.marga@gmail.com>
2018-10-15 17:29:26 +0300

    Use decodedJobName to avoid %2F
    Refactor message summary (WIP)

--- vars/notify.groovy
+++ vars/notify.groovy
@@ -12,13 +12,15 @@ def call(String buildStatus = 'STARTED') {
   // def gitCommitShortSHA=sh(returnStdout: true, script: 'git rev-parse --short HEAD')
   // GIT_COMMIT_MESSAGE=sh(returnStdout: true, script: 'git log -1 --format=%B ${env.GIT_COMMIT}').trim()
   // GIT_COMMIT_SHORT=sh(returnStdout: true, script: 'git rev-parse --short HEAD')
+  def decodedJobName = env.JOB_NAME.replaceAll("%2F", "/")
   def colorName = 'RED'
   def colorCode = '#FF0000'
-  def subject = "${buildStatus}: build in '${env.JOB_NAME} [${env.BUILD_NUMBER}]'"
-  def summary = "${subject} \n Stage: '${env.STAGE_NAME}' (<${env.BUILD_URL}|Open>)"
-  def details = """<p>${buildStatus}: Job '${env.JOB_NAME} [${env.BUILD_NUMBER}]':</p>
+  def subject = "${buildStatus}: build in '${decodedJobName}' [<${env.BUILD_URL}|${env.BUILD_NUMBER}>]"
+  // def summary = "${subject} \n Stage: '${env.STAGE_NAME}' (<${env.BUILD_URL}|Open>)"
+  def summary = "${subject}"
+  def details = """<p>${buildStatus}: Job '${decodedJobName} [${env.BUILD_NUMBER}]':</p>
     <p>STAGE: env.STAGE_NAME</p>
-    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${env.JOB_NAME} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
+    <p>Check console output at &QUOT;<a href='${env.BUILD_URL}'>${decodedJobName} [${env.BUILD_NUMBER}]</a>&QUOT;</p>"""
 
   // Override default values based on build status
   if (buildStatus == 'STARTED') {
